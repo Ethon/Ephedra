ae9737660c22d79793f34059e609eb063e80f061
Florian Erler <ethon@ethon.cc>
2012-09-26 10:06:12 +0200

    Fixed license and added guards.

--- ephedra/detail/proxy.hpp
+++ ephedra/detail/proxy.hpp
@@ -1,19 +1,29 @@
-/*
- * This file is part of Foobar.
+/* All copyright belongs to Florian Erler <ethon[@]ethon.cc>
  * 
- * Foobar is free software: you can redistribute it and/or modify
+ * This file is part of Ephedra.
+ * 
+ * Ephedra is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
 
- * Foobar is distributed in the hope that it will be useful,
+ * Ephedra is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
 
  * You should have received a copy of the GNU General Public License
- * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Ephedra.  If not, see <http://www.gnu.org/licenses/>.
  */
+ 
+ // MSVC-Guard
+#ifdef _MSC_VER
+#	paragma once
+#endif // _MSC_VER
+
+// Guard
+#ifndef HEADER_UUID_7B6158160079453BA34B55220889D824
+#define HEADER_UUID_7B6158160079453BA34B55220889D824
 
 namespace ephedra
 {
@@ -38,3 +48,5 @@ namespace ephedra
 		}
 	}
 }
+
+#endif // HEADER_UUID_7B6158160079453BA34B55220889D824
